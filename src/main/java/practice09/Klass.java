a85906e9991db309d4cd3df2a2c9af84fd1a3db2
MARTIN ZHENG (ITA-ISDC-ISD-OOCLL/ZHA) <ZHENGMA3@OOCL.COM>
2020-07-22 20:12:08 +0800

    [Martin] feature: add implements for practice09

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,42 @@
 package practice09;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+
+    private int number;
+    private Student leader;
+    private List<Student> students;
+
+    public Klass(int number) {
+        this.number = number;
+        this.students = new ArrayList<>();
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", this.number);
+    }
+
+    public void assignLeader(Student student) {
+        if (this.students.size() == 0) {
+            System.out.println("It is not one of us.");
+            return;
+        }
+        this.leader = student;
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public void appendMember(Student student) {
+        this.students.add(student);
+    }
+
 }
